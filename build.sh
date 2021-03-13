2cecff2932a0600d6c21d64980de43c1312e053b
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2021-03-13 13:14:57 +0100

    Move board code under src/board

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ ROOT_DIR=$(dirname "$0")
 
 arduino-cli compile \
     --fqbn arduino:avr:uno \
-    --build-property "compiler.cpp.extra_flags=-I${ROOT_DIR}/src" \
+    --build-property "compiler.cpp.extra_flags=-I${ROOT_DIR}/src/board" \
     --output-dir "${ROOT_DIR}/bin"
