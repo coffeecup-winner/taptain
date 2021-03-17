497157ceebe83344c6435ebb8ae37fa637fd3001
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2021-03-17 01:16:42 +0100

    Introduce init handler

--- src/events.rs
+++ src/events.rs
@@ -2,6 +2,7 @@ use crate::{comms::Connection, protocol::Message};
 
 pub struct EventProcessor {
     connection: Connection,
+    init_handler: Box<dyn FnMut(&mut Connection) -> std::io::Result<()>>,
     launch_handler: Box<dyn FnMut(&mut Connection, u8) -> std::io::Result<()>>,
 }
 
@@ -9,8 +10,16 @@ impl EventProcessor {
     pub fn new(connection: Connection) -> EventProcessor {
         EventProcessor {
             connection,
+            init_handler: Box::new(|_| Ok(())),
             launch_handler: Box::new(|_, _| Ok(())),
         }
+    }
+
+    pub fn register_init_handler(
+        &mut self,
+        handler: Box<dyn FnMut(&mut Connection) -> std::io::Result<()>>,
+    ) {
+        self.init_handler = handler;
     }
 
     pub fn register_launch_handler(
@@ -21,17 +30,14 @@ impl EventProcessor {
     }
 
     pub fn run_event_loop(&mut self) -> std::io::Result<()> {
+        (self.init_handler)(&mut self.connection)?;
         loop {
             let message = dbg!(self.connection.get_next_message()?);
-            self.dispatch(message)?;
-        }
-    }
-
-    fn dispatch(&mut self, message: Message) -> std::io::Result<()> {
-        match message {
-            Message::OK => panic!("Programmer error: should be handled elsewhere"),
-            Message::Error => panic!("Programmer error: should be handler elsewhere"),
-            Message::Launch(i_widget) => (self.launch_handler)(&mut self.connection, i_widget),
+            match message {
+                Message::OK => panic!("Programmer error: should be handled elsewhere"),
+                Message::Error => panic!("Programmer error: should be handler elsewhere"),
+                Message::Launch(i_widget) => (self.launch_handler)(&mut self.connection, i_widget)?,
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,23 @@ use comms::Connection;
 use events::EventProcessor;
 use protocol::Command;
 
-fn start(mut connection: Connection) -> std::io::Result<()> {
-    connection.send(&[
-        Command::Init(0, "01234567890123456789".to_owned()),
-        Command::Init(1, "TEST WIDGET".to_owned()),
-        Command::Init(2, "MORE TESTS".to_owned()),
-        Command::Init(3, "RUN A PROCESS".to_owned()),
-        Command::Init(4, "BREW SOME TEA".to_owned()),
-        Command::Init(5, "LAUNCH THE SATELLITE".to_owned()),
-    ])?;
+fn start(mut event_processor: EventProcessor) -> std::io::Result<()> {
+    event_processor.register_init_handler(Box::new(|connection| {
+        connection.send(&[
+            Command::Init(0, "01234567890123456789".to_owned()),
+            Command::Init(1, "TEST WIDGET".to_owned()),
+            Command::Init(2, "MORE TESTS".to_owned()),
+            Command::Init(3, "RUN A PROCESS".to_owned()),
+            Command::Init(4, "BREW SOME TEA".to_owned()),
+            Command::Init(5, "LAUNCH THE SATELLITE".to_owned()),
+        ])?;
+        Ok(())
+    }));
 
-    let mut event_processor = EventProcessor::new(connection);
-
-    event_processor.register_launch_handler(Box::new(|conn, i_widget| {
+    event_processor.register_launch_handler(Box::new(|connection, i_widget| {
         for i in 1..=10 {
             std::thread::sleep(std::time::Duration::from_millis(100));
-            conn.send(&[Command::Progress(i_widget, i * 10)])?;
+            connection.send(&[Command::Progress(i_widget, i * 10)])?;
         }
         Ok(())
     }));
@@ -33,7 +34,7 @@ fn main() {
     match std::env::args().nth(1) {
         Some(arg) => {
             let connection = Connection::open(&arg).expect("Failed to open a connection");
-            start(connection).expect("Port communication failed");
+            start(EventProcessor::new(connection)).expect("Port communication failed");
         }
         None => {}
     }
